19a21b31f7f220fadc0793e407098f61a438fe77
techmaxk2 <47863792+techmaxk2@users.noreply.github.com>
2019-02-23 20:01:53 +0300

    add forgoten !

--- src/main/java/tutorial/Hello.java
+++ src/main/java/tutorial/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("new out");
-        System.out.println("last out");
+        System.out.println("last out!");
     }
 }
